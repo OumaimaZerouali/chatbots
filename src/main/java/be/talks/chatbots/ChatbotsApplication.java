affd4f34e9922f6b103c10e39b652abc467d74d3
oumaimazerouali <oumaima.zerouali@ordina.be>
2025-10-23 15:28:03 +0200

    feat(start): Setup the project

---
+++ src/main/java/be/talks/chatbots/ChatbotsApplication.java
@@ -0,0 +1,13 @@
+package be.talks.chatbots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/talks/chatbots/ChatbotsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.talks.chatbots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
